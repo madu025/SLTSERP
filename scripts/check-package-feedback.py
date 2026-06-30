97abc2878547140ac0a4f179e247aa85d04429ad
Prasad Maduranga Dissanayake <prasadexperior@gmail.com>
2026-06-30 19:39:07 +0530

    Remove Unawanted

--- scripts/check-package-feedback.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('tmp_feedback_v2.json', 'r') as f:
-    fb = json.load(f)
-    
-layers = fb['outputs']['qfield_layer_data']['layers_by_id']
-for lid, ldata in layers.items():
-    if ldata.get('name', '').startswith('SLT_'):
-        print(f"Layer: {ldata.get('name')} -> qfs_action: {ldata.get('qfs_action')}")
