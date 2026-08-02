80ea906edd41fc81853cb60658cdc515983e370f
Prasad Maduranga Dissanayake <prasadexperior@gmail.com>
2026-08-02 21:18:04 +0530

    chore: remove temporary debug and check scripts from scripts directory

--- scripts/check.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../src/lib/prisma';
-async function run() {
-  const req = await prisma.stockRequest.findFirst({
-    where: { requestNr: "PRN-20260801-5772" },
-    include: { purchaseOrders: true }
-  });
-  console.log(JSON.stringify(req, null, 2));
-}
-run().finally(() => prisma.$disconnect());
--- scripts/clear-gl.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  await prisma.gLMappingConfig.deleteMany({});
-  console.log('Cleared GLMappingConfig');
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
